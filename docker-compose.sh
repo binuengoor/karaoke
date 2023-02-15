47bf7a23b9ce2f7df8cd186afebb14e519a00067
bp <3millionmax@gmail.com>
2023-02-15 15:27:11 +0000

    docker compose script updated

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.selfhost.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.selfhost.yml up -d
